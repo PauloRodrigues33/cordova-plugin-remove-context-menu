f54a2f7b86d15e7d5eaeabd9580c5f3dad702831
Paulo Rodrigues <paulo.robertojr33@gmail.com>
2018-01-26 12:51:04 -0200

    androidDisableShareButton

--- src/android/cordovacontextbuttons.java
+++ src/android/cordovacontextbuttons.java
@@ -1,7 +1,21 @@
 package com.prodrigues.cordovacontextbuttons;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.medgrupo.medsoft.pro.R;
+
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaInterfaceImpl;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
@@ -9,19 +23,74 @@ import org.json.JSONException;
 
 public class cordovacontextbuttons extends CordovaPlugin {
 
+  Activity activity;
+  Menu _customMenu;
+  ContextMenu _contextMenu;
+  CordovaWebView view;
+
+  boolean isDisableShareButton = false;
+  boolean isDisableSearchButton = false;
+
   @Override
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
       super.initialize(cordova, webView);
-      // your init code here
+      this.activity = cordova.getActivity();
+      this.view = webView;
   }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-      if ("beep".equals(action)) {
-          callbackContext.success();
-          return true;
+    try {
+      if ("disableShareButton".equals(action)) {
+        boolean status = setShareButtonVisible(Boolean.parseBoolean(args.getString(0)));
+        if (status) {
+          callbackContext.success("Share button disabled");
+        } else {
+          callbackContext.error("Share button enabled");
+        }
+        callbackContext.success();
+        return true;
+      }
+      if ("disableSearchButton".equals(action)) {
+        callbackContext.success();
+        return true;
       }
       return false;  // Returning false results in a "MethodNotFound" error.
+    }
+    catch (Exception e){
+      callbackContext.error(e.getMessage());
+      return false;
+    }
   }
 
+  private boolean setShareButtonVisible(boolean visible){
+    this.isDisableShareButton = visible;
+    return visible;
+  }
+
+  @Override
+  public Object onMessage(String id, Object data) {
+    if("onPrepareOptionsMenu".equals(id)){
+      this._customMenu = rebuildMenu((Menu) data);
+    }
+    return super.onMessage(id, this._customMenu);
+  }
+
+  private Menu rebuildMenu(Menu menu){
+    if(this.isDisableShareButton){
+      MenuItem share = menu.findItem(R.id.share_clipboard_button);
+      if(share != null){
+        share.setVisible(false);
+      }
+      MenuItem share2 = menu.findItem(R.id.share_app_button);
+      if(share2 != null){
+        share2.setVisible(false);
+      }
+      MenuItem share3 = menu.findItem(R.id.share_bookmark_button);
+      if(share3 != null){
+        share3.setVisible(false);
+      }
+    }
+    return menu;
+  }
 }
