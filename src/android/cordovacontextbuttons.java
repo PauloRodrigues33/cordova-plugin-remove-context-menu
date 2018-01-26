074bb3d3dc877843adae0a09437c577fc1c68b08
Paulo Rodrigues <paulo.robertojr33@gmail.com>
2018-01-26 11:07:05 -0200

    update

--- src/android/cordovacontextbuttons.java
+++ src/android/cordovacontextbuttons.java
@@ -1,35 +1,27 @@
-// package src.android.cordovacontextbuttons;
+package com.prodrigues.cordovacontextbuttons;
 
-// public class cordovacontextbuttons extends CordovaPlugin{
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-// @Override
-// public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-//     super.initialize(cordova, webView);
-//     // your init code here
-// }
+public class cordovacontextbuttons extends CordovaPlugin {
 
-// @Override
-// public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-//     if ("beep".equals(action)) {
-//         this.beep(args.getLong(0));
-//         callbackContext.success();
-//         return true;
-//     }
-//     return false;  // Returning false results in a "MethodNotFound" error.
-// }
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+      super.initialize(cordova, webView);
+      // your init code here
+  }
 
-// @Override
-// public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-//     if ("beep".equals(action)) {
-//         final long duration = args.getLong(0);
-//         cordova.getActivity().runOnUiThread(new Runnable() {
-//             public void run() {
-//                 ...
-//                 callbackContext.success(); // Thread-safe.
-//             }
-//         });
-//         return true;
-//     }
-//     return false;
-// }
-// } 
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+      if ("beep".equals(action)) {
+          callbackContext.success();
+          return true;
+      }
+      return false;  // Returning false results in a "MethodNotFound" error.
+  }
+
+}
