809e20d36498b5308a266004a3a7634342aa76b4
Paulo Rodrigues <paulo.robertojr33@gmail.com>
2018-01-26 13:59:45 -0200

    update

--- www/cordovacontextbuttons.js
+++ www/cordovacontextbuttons.js
@@ -1,11 +1,18 @@
-function Cordovacontextbuttons(){
-    var RemoveAllButtons = function(){
-        cordova.exec(function(success){
+var cordovacontextbuttons = {
+    removeAllButtons = function () {
+        cordova.exec(function (success) {
 
-        }, function(fail){
-            
+        }, function (fail) {
+
         }, "cordovacontextbuttons", "removebuttons");
+    },
+    setShareButtonVisible = function (visible) {
+        cordova.exec(function (success) {
+            console.log("Share button disabled");
+        }, function (fail) {
+            console.log("Share button enabled");
+        }, "cordovacontextbuttons", "disableShareButton", [visible]);
     }
 }
 
-module.exports = new Cordovacontextbuttons();
+module.exports = cordovacontextbuttons;
